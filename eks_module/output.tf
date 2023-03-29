927c71e63e7b91ecef8fe89b39c0978aa454ff07
camilaqueiroz-mck <camilaqueiroz-mck@mckinsey.com>
2023-03-29 15:46:02 -0300

    teste

--- eks_module/output.tf
+++ eks_module/output.tf
@@ -1,4 +1,3 @@
 output "eks_sec_group_id" {
-#   value = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
- value = "2"
+    value = aws_eks_cluster.eks_cluster.vpc_config[0].security_group_ids
 }
