1bb61dc3af141c72100d172b46a16599030ee2d5
camilaqueiroz-mck <camilaqueiroz-mck@mckinsey.com>
2023-03-29 15:37:34 -0300

    coloca valor 2

--- eks_module/output.tf
+++ eks_module/output.tf
@@ -1,3 +1,4 @@
 output "eks_sec_group_id" {
-  value = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+#   value = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+ value = "2"
 }
