06618711ab9b894532a249bc35b2bcdd46ba09bc
camilaqueiroz-mck <camilaqueiroz-mck@mckinsey.com>
2023-03-30 13:49:22 -0300

    altera erros

--- rds_module/main.tf
+++ rds_module/main.tf
@@ -10,6 +10,8 @@ resource "aws_db_instance" "default" {
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.subnet_db_group.id
   vpc_security_group_ids = [aws_security_group.security_group_db.id]
+  backup_retention_period = "30"
+  performance_insights_enabled = true
 }
 
 resource "aws_security_group" "security_group_db" {
@@ -25,10 +27,11 @@ resource "aws_security_group" "security_group_db" {
     security_groups = [var.eks_sec_group_id]
   }
   egress {
+    description      = "TLS from DB"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    security_groups = [var.eks_sec_group_id]
   }
   tags = {
     Name = "security_group_db_${var.env}"
